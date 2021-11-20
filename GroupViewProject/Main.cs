50df693565b549aef2cffdefeee892a04197bd1e
Alexander <sanyatipkov@gmail.com>
2021-11-20 13:57:08 +0900

    Initial commit

---
+++ GroupViewProject/Main.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace GroupViewProject
+{
+    class Main
+    {
+
+        // Строка подключения к БД
+        string strConnection = @"Data Source=DESKTOP-7GLORK1\SQLEXPRESS;Initial Catalog=groupall;Integrated Security=True";
+        
+        /// <summary>
+        /// Добавления данных о группе 
+        /// </summary>
+        /// <param name="group"> Класс Group</param>
+        public void AddGroup (Group group)
+        {
+            // 
+            string SqlCmd = $"INSERT INTO [dbo].[group] ([NameGroup],[NumberGroup],[CuratorGroup]) " +
+                $"VALUES ('{group.NameGroup}','{group.NumberGroup}','{group.CuratorGroup}')";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Группа добавлена");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        /// <summary>
+        /// Чтения данных из таблицы group
+        /// </summary>
+        /// <returns></returns>
+        public List<Group> ReadGroup()
+        {
+            string SqlCmd = "SELECT * FROM [dbo].[group]";
+            List<Group> groups = new List<Group>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+
+                    // Получаем строки из таблицы
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+                    
+                    if (sqlDataReader.HasRows)
+                    {
+                        // Построчно считываем данные
+                        while (sqlDataReader.Read())
+                        {
+                            groups.Add(new Group()
+                            {
+                                idGroup = sqlDataReader.GetInt32(0),
+                                NameGroup = sqlDataReader.GetString(1),
+                                NumberGroup = sqlDataReader.GetString(2),
+                                CuratorGroup = sqlDataReader.GetString(3)
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            return groups;
+        }
+        public void DelGroup(Group group)
+        {
+            string SqlCmd = $"Delete From [dbo].[group] Where idGroup={group.idGroup};";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Группа удалена");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        public void UpdGroup(Group group)
+        {
+            string SqlCmd = $"Update [dbo].[group] set NameGroup='{group.NameGroup}'  ,NumberGroup ='{group.NumberGroup}',CuratorGroup ='{group.CuratorGroup}' Where idGroup={group.idGroup};";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Группа изменена");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+    }
+
+
+
+
+    /// <summary>
+    ///  Описание таблицы из БД
+    /// </summary>
+    class Group
+    {
+        public int idGroup { get; set; }
+
+        public string  NameGroup { get; set; }
+
+        public string NumberGroup { get; set; }
+
+        public string  CuratorGroup { get; set; }
+    }
+}
---
+++ GroupViewProject/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupViewProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdListView();
+        }
+        String Idgroup;
+        void UpdListView()
+        {
+            Main main = new Main();
+
+            dbViewGroup.ItemsSource = main.ReadGroup();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(tbNameGroup.Text) && !String.IsNullOrEmpty(tbNumberGroup.Text) && !String.IsNullOrEmpty(tbCurator.Text))
+                {
+                    Group group = new Group()
+                    {
+                        NameGroup = tbNameGroup.Text,
+                        NumberGroup = tbNumberGroup.Text,
+                        CuratorGroup = tbCurator.Text
+                    };
+
+                    Main main = new Main();
+
+                    main.AddGroup(group);
+                    UpdListView();
+                }
+                else MessageBox.Show("Одно из полей пустое");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(tbNameGroup.Text))
+                {
+                    Group group = new Group()
+                    {
+                        idGroup = Convert.ToInt32(Idgroup)
+                    };
+                    Main main = new Main();
+
+                    main.DelGroup(group);
+                    UpdListView();
+                }
+                else MessageBox.Show("Одно из полей пустое");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(tbNameGroup.Text) && !String.IsNullOrWhiteSpace(tbNumberGroup.Text) && !String.IsNullOrWhiteSpace(tbCurator.Text) && !String.IsNullOrWhiteSpace(Idgroup))
+                {
+                    Group group = new Group()
+                    {
+                        idGroup = Convert.ToInt32(Idgroup),
+                        NameGroup = tbNameGroup.Text,
+                        NumberGroup = tbNumberGroup.Text,
+                        CuratorGroup = tbCurator.Text
+                    };
+                    Main main = new Main();
+                    main.UpdGroup(group);
+                    UpdListView();
+                }
+                else
+                {
+                    MessageBox.Show("Одно из полей пустое");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void lvViewGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Group group = new Group();
+            group = dbViewGroup.SelectedItem as Group;
+            if (group != null)
+            {
+
+                Idgroup = group.idGroup.ToString();
+                tbNameGroup.Text = group.NameGroup;
+                tbNumberGroup.Text = group.NumberGroup;
+                tbCurator.Text = group.CuratorGroup;
+            }
+        }
+    }
+}
